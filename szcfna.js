78020fa17900e65bd72193015c2731a22a2357d4
Kenny Jiang <kenny.jiang@zoom.us>
2020-09-16 16:15:14 +0800

    szcfna monitor

--- szcfna.js
+++ szcfna.js
@@ -1,20 +1,19 @@
-const { logger } = require('./lib/logger');
 const SzfcClient = require('./lib/szfc');
+const QyWechat = require("./lib/qywechat")
 const _ = require('lodash');
 const { table } = require('table');
 const async = require('async');
 const process = require('process');
+const { logger } = require('./lib/logger');
 
 const RETRY_MAP = {};
 let totalHouseList = [];
-let startTimeStamp = 0;
-let unsellCount = 0;
-let sellingCount = 0;
-let soldCount = 0;
-let disableCount = 0;
-
 let projectInterval = 1000;
 let buildingInterval = 2000;
+
+const STATUS_ARRAY = ['可售出', '签约中', '已售出', '锁定中'];
+const COMPARE_MAP = new Map();
+const qywechat = new QyWechat('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=7aa7561c-a218-4661-af98-a43d2a4034a8');
 
 const projectQueue = async.queue(function(project, callback) {
   const buildingQueue = async.queue(function({
@@ -43,6 +42,9 @@ const projectQueue = async.queue(function(project, callback) {
   
   const saveHouseList = function(err, houseList) {
     totalHouseList = _.uniqBy(_.concat(totalHouseList, houseList), 'identifier');
+    _.map(houseList, function(house) {
+      compareHouse(house);
+    })
   }
 
   setTimeout(async ()=> {
@@ -73,17 +75,27 @@ projectQueue.error(function(err, project) {
   logger.error(`重试 [${project.name}] 第 ${retryCount + 1} 次`);
 });
 
+const compareHouse = function(house) {
+  // If exist house status, 
+  if (COMPARE_MAP.has(house.identifier)) {
+    let cachedHouse = COMPARE_MAP.get(house.identifier);
+    if (cachedHouse.houseStatus != house.houseStatus) {
+      // House status changed
+      logger.info(`${house.identifier} 状态变化, 原状态 [${STATUS_ARRAY[cachedHouse.houseStatus]}] -> 现状态 [${STATUS_ARRAY[house.houseStatus]}]`);
+      qywechat.sendMarkdown(`**${house.identifier} 状态变化!!!**\n原状态 [${STATUS_ARRAY[cachedHouse.houseStatus]}] \n现状态 [${STATUS_ARRAY[house.houseStatus]}]`)
+    } else {
+      logger.debug(`${house.identifier} 没有变化`);
+    }
+  }
+
+  COMPARE_MAP.set(house.identifier, house);
+}
+
 const calcResult = function() {
-  let endTimeStamp = new Date().getTime();
-
-  logger.info(`Project queue: ${projectQueue.length()}`);
-  logger.info(`花费时间: ${(endTimeStamp - startTimeStamp)/1000} 秒`);
-  logger.info(`取得数据: ${totalHouseList.length} 条`);
-
-  unsellCount += _.filter(totalHouseList, {houseStatus: 0}).length;
-  sellingCount += _.filter(totalHouseList, {houseStatus: 1}).length;
-  soldCount += _.filter(totalHouseList, {houseStatus: 2}).length;
-  disableCount += _.filter(totalHouseList, {houseStatus: 3}).length;
+  let unsellCount = _.filter(totalHouseList, {houseStatus: 0}).length;
+  let sellingCount = _.filter(totalHouseList, {houseStatus: 1}).length;
+  let soldCount = _.filter(totalHouseList, {houseStatus: 2}).length;
+  let disableCount = _.filter(totalHouseList, {houseStatus: 3}).length;
 
   let countTableData = [];
   countTableData.push(['可售出', unsellCount]);
@@ -93,68 +105,30 @@ const calcResult = function() {
   countTableData.push(['合计', totalHouseList.length]);
 
   logger.info("\n" + table(countTableData));
+  qywechat.sendMarkdown(table(countTableData));
 }
 
-process.on('SIGINT', function() {
-  process.exit();
-});
+async function startWatching() {
+  // Random next tick
+  let nextTick = 30 * 1000 + _.random(30 * 1000);
+  logger.info("next tick", nextTick);
+  setTimeout(startWatching, nextTick);
 
-process.on('exit', function() {
-  calcResult();
-});
+  try {
+    await projectQueue.push([{
+      id: 'b040951a-3028-4b95-b6a2-439e019d013e',
+      name: '春风南岸花园一期',
+      district: '高新区'
+    }, {
+      id: '4e5947bb-75a9-4a09-8167-291c7d16a3e2',
+      name: '春风南岸花园二期',
+      district: '高新区'
+    }]);
 
-startTimeStamp = new Date().getTime();
+    calcResult();
+  } catch (e) {
+    logger.error(e);
+  }
+}
 
-(async function () {
-  let szfcClient = await new SzfcClient();
-
-  let districtList = await szfcClient.getDistrictList();
-  logger.debug(`取得 ${districtList.length} 个地区`);
-
-  _(districtList).forEach(async function(district) {
-    let pageSize = await szfcClient.getProjectCount(district);
-
-    logger.info(`${district} 共有 ${pageSize} 页数据`);
-
-    // Loop project list page
-    for (let i=1; i<= pageSize; i++) {
-      let projectList = await szfcClient.getProjectList(district, i);
-      logger.debug(`从 ${district} 第 ${i} 页取得 ${projectList.length} 条数据`);
-
-      // Loop building under project
-      _(projectList).forEach(async function(project) {
-        await projectQueue.push(project);
-      });
-    }
-  });
-}());
-
-// (async function () {
-//   await szfc.getSaleInfoProListIndex();
-
-//   projectQueue.push([{
-//     id: 'b040951a-3028-4b95-b6a2-439e019d013e',
-//     name: '春风南岸花园一期',
-//     district: '高新区'
-//   }, {
-//     id: '4e5947bb-75a9-4a09-8167-291c7d16a3e2',
-//     name: '春风南岸花园二期',
-//     district: '高新区'
-//   }]);
-
-//   await Promise.all([projectQueue.drain(), buildingQueue.drain()]);
-// }());
-
-exports.main_handler = async (event, context) => {
-  await projectQueue.push([{
-    id: 'b040951a-3028-4b95-b6a2-439e019d013e',
-    name: '春风南岸花园一期',
-    district: '高新区'
-  }, {
-    id: '4e5947bb-75a9-4a09-8167-291c7d16a3e2',
-    name: '春风南岸花园二期',
-    district: '高新区'
-  }]);
-};
-
-// exports.main_handler({}, {}, function() {});
+startWatching();

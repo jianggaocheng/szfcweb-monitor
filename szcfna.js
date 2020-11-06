5184461ca4674924cc498ad5cd68db25863fd909
Kenny Jiang <kenny.jiang@zoom.us>
2020-11-06 18:00:58 +0800

    add daily report

--- szcfna.js
+++ szcfna.js
@@ -1,8 +1,9 @@
 const SzfcClient = require('./lib/szfc');
-const QyWechat = require("./lib/qywechat")
+const QyWechat = require("./lib/qywechat");
 const _ = require('lodash');
 const { table } = require('table');
 const async = require('async');
+const schedule = require('node-schedule');
 const process = require('process');
 const { logger } = require('./lib/logger');
 
@@ -14,6 +15,18 @@ let buildingInterval = 2000;
 const STATUS_ARRAY = ['可售出', '签约中', '已售出', '锁定中'];
 const COMPARE_MAP = new Map();
 const qywechat = new QyWechat('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=7aa7561c-a218-4661-af98-a43d2a4034a8');
+
+const PROJECT_LIST = [
+  {
+    id: 'b040951a-3028-4b95-b6a2-439e019d013e',
+    name: '春风南岸花园一期',
+    district: '高新区'
+  }, {
+    id: '4e5947bb-75a9-4a09-8167-291c7d16a3e2',
+    name: '春风南岸花园二期',
+    district: '高新区'
+  }
+];
 
 const projectQueue = async.queue(function(project, callback) {
   const buildingQueue = async.queue(function({
@@ -102,6 +115,10 @@ const compareHouse = function(house) {
 }
 
 const calcResult = function() {
+  if(_.isEmpty(totalHouseList)) {
+    return;
+  }
+
   _.each(totalHouseList, function(house) {
     compareHouse(house);
   })
@@ -128,21 +145,40 @@ async function startWatching() {
   setTimeout(startWatching, nextTick);
 
   try {
-    await projectQueue.push([{
-      id: 'b040951a-3028-4b95-b6a2-439e019d013e',
-      name: '春风南岸花园一期',
-      district: '高新区'
-    }, {
-      id: '4e5947bb-75a9-4a09-8167-291c7d16a3e2',
-      name: '春风南岸花园二期',
-      district: '高新区'
-    }]);
-
+    await projectQueue.push(PROJECT_LIST);
     calcResult();
-    totalHouseList = [];
   } catch (e) {
     logger.error(e);
   }
 }
 
 startWatching();
+
+// Daily report
+schedule.scheduleJob('0 0 22 * * *', () => {
+  if (_.isEmpty(totalHouseList)) {
+    log.info('ignore empty house list');
+  }
+
+  let now = new Date();
+  let report = `** ${now.getMonth()+1}.${now.getDate()} 销售日报 **\n`;
+
+  for (let i=0; i<PROJECT_LIST.length; i++) {
+    let project = PROJECT_LIST[i];
+    let totalCount = _.filter(totalHouseList, (value) => value.project.id == project.id).length;
+    let unsellCount = _.filter(totalHouseList, {houseStatus: 0, project: project}).length;
+    
+    let sellProgress = _.round(1 - (unsellCount/totalCount), 2);
+    logger.info(`${project.name} 总共: [${totalCount}] 已售出: [${totalCount-unsellCount}] 销售率: [${sellProgress}]`);
+    report += `${project.name} \n总共: [${totalCount}] \n已售出: [${totalCount-unsellCount}] \n销售率: [${sellProgress}]\n\n\n`
+    // qywechat.sendMarkdown(`**${house.project.name} ${house.building.name}栋 ${house.houseSN}室 状态变化!!!**\n原状态 [${STATUS_ARRAY[cachedHouse.houseStatus]}] \n现状态 [${STATUS_ARRAY[house.houseStatus]}]`); 
+  }
+
+  let totalCount = totalHouseList.length;
+  let unsellCount = _.filter(totalHouseList, {houseStatus: 0}).length;
+  let sellProgress = _.round(1 - (unsellCount/totalCount), 2);
+  logger.info(`合计总共: [${totalCount}] 已售出: [${totalCount-unsellCount}] 销售率: [${sellProgress}]`);
+  report += `合计总共: [${totalCount}] \n已售出: [${totalCount-unsellCount}] \n销售率: [${sellProgress}]\n`;
+
+  qywechat.sendMarkdown(report);
+});
